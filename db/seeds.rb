dbd0e7012ee9189fbe80ad3be487bd9b72756bd7
Vinicius Rocha <vinicius.rocha@shopify.com>
2024-02-21 22:02:15 -0500

    Display list of projects with progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+COUNT_PROJECT = 30
+COUNT_TASKS_PER_PROJECT = 20
+
+COUNT_PROJECT.times do |i|
+  project = Project.create!(name: "Project #{i + 1}")
+
+  COUNT_TASKS_PER_PROJECT.times do |j|
+    project.tasks.create!(title: "Task #{j + 1}", completed: [true, false].sample)
+  end
+end
