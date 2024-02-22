5e54dc2d70d076cf6159f5e13fd29a0d3e34f7bf
Vinicius Rocha <vinicius.rocha@shopify.com>
2024-02-21 23:26:20 -0500

    Move tasks under project route

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -6,43 +6,43 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get tasks_url
+    get project_tasks_url(@task.project)
     assert_response :success
   end
 
   test "should get new" do
-    get new_task_url
+    get new_project_task_url(@task.project)
     assert_response :success
   end
 
   test "should create task" do
     assert_difference("Task.count") do
-      post tasks_url, params: { task: { completed: @task.completed, project_id: @task.project_id, title: @task.title } }
+      post project_tasks_url(@task.project), params: { task: { completed: @task.completed, project_id: @task.project_id, title: @task.title } }
     end
 
-    assert_redirected_to task_url(Task.last)
+    assert_redirected_to project_url(Task.last.project)
   end
 
   test "should show task" do
-    get task_url(@task)
+    get project_task_url(@task.project_id, @task.id)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_task_url(@task)
+    get edit_project_task_url(@task.project_id, @task.id)
     assert_response :success
   end
 
   test "should update task" do
-    patch task_url(@task), params: { task: { completed: @task.completed, project_id: @task.project_id, title: @task.title } }
-    assert_redirected_to task_url(@task)
+    patch project_task_url(@task.project_id, @task.id), params: { task: { completed: @task.completed, project_id: @task.project_id, title: @task.title } }
+    assert_redirected_to project_url(@task.project)
   end
 
   test "should destroy task" do
     assert_difference("Task.count", -1) do
-      delete task_url(@task)
+      delete project_task_url(@task.project_id, @task.id)
     end
 
-    assert_redirected_to tasks_url
+    assert_redirected_to project_url(@task.project)
   end
 end
