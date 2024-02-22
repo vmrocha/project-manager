46ebbd47072ecfbb45b42c320f684d66faefe07c
Vinicius Rocha <vinicius.rocha@shopify.com>
2024-02-21 22:26:57 -0500

    Display tasks under project view

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,12 @@
 module ProjectsHelper
+  def progress_bar(project)
+    display_text = "#{project.completion_ratio}%"
+    color = project.completion_ratio < 1 ? "is-warning" : "is-success"
+    tag.progress(
+      display_text,
+      value: project.tasks.completed.count,
+      max: project.tasks.count,
+      class: "progress #{color}"
+    )
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
+
+  def completion_ratio
+    return 0 if tasks.count.zero?
+
+    tasks.completed.count.to_f / tasks.count
+  end
 end
